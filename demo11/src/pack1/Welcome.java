4559df24ccef59a0fba40b64519a8a6d2a5b47da
saicharan <219x1a3364@gprec.ac.in>
2023-10-30 11:59:56 +0530

    Second commit

--- demo11/src/pack1/Welcome.java
+++ demo11/src/pack1/Welcome.java
@@ -1,10 +1,7 @@
 package pack1;
-
 public class Welcome {
-
 	public static void main(String[] args) {
 		System.out.println("Hi welcome to ITCA");
-
+		System.out.println("Greetings from GPREC");
 	}
-
 }
